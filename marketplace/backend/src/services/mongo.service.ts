296e5390332e4a34812ec707d394f143edfc9b77
LeftBrainCreated <leftbraincreated@gmail.com>
2023-11-29 21:59:46 -0500

    added mongo

--- marketplace/backend/src/services/mongo.service.ts
+++ marketplace/backend/src/services/mongo.service.ts
@@ -7,7 +7,7 @@ const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PW = process.env.MONGO_PW;
 const { MongoClient } = require('mongodb');
 
-const url = `mongodb://${MONGO_USER}:${MONGO_PW}@localhost:27017/`;
+const url = `mongodb://${MONGO_USER}:${MONGO_PW}@mongo:27017/`;
 
 export async function connectToDatabase() {
     dotenv.config();
